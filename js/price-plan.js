d44cb1fd2ba4149b0d9df067f23df3f42608138a
Patrick Machado <patrick@ilhasoftware.com.br>
2017-09-28 14:01:02 -0300

    Total do plano anual e ajustes de layout

--- js/price-plan.js
+++ js/price-plan.js
@@ -3,8 +3,13 @@ $(".pricing .full-container .plan .contratar select")
 				function(e) {
 					var option = this.options[e.target.selectedIndex];
 					var planId = option.getAttribute("data-plan");
-					var amount = $(".pricing .full-container .plan .price .amount[data-plan="
-							+ planId + "]");
+					var planTotal = $(".pricing .full-container .plan[data-plan=" + planId + "] .plan-detail .contratar .plan-total p");
+					if (option.value != "anual") {
+						planTotal.css("display", "none");
+					} else {
+						planTotal.css("display", "block");
+					}
+					var amount = $(".pricing .full-container .plan[data-plan=" + planId + "] .price .amount");
 					var price = option.getAttribute("data-price"); 
 					amount[0].innerHTML = price;
 				});
